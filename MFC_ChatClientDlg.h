db457e958494eed41f4179ac57b01e24d0cc6215
hitblda <hitblda@163.com>
2019-11-18 14:49:22 +0800

    添加项目文件。

---
+++ MFC_ChatClientDlg.h
@@ -0,0 +1,43 @@
+﻿
+// MFC_ChatClientDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CMFCChatClientDlg 对话框
+class CMFCChatClientDlg : public CDialogEx
+{
+// 构造
+public:
+	CMFCChatClientDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFC_CHATCLIENT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnCbnSelchangeCombo1();
+	afx_msg void OnEnChangeEdit1();
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnLbnSelchangeList1();
+	afx_msg void OnBnClickedButton4();
+	afx_msg void OnBnClickedButton3();
+	afx_msg void OnBnClickedClear();
+};
